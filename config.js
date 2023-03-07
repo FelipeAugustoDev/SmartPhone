9c7d8752d86ac6e70d29a4e734025b1ffe9404c7
Felipe Augusto <felipeaugustob.dealmeida@gmail.com>
2023-03-06 22:10:11 -0300

    Algumas mudancas que estao me deixando maluco

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
-
 show = true;
-const telao = document.getElementById('workspace');
-const botaozin = document.getElementById('macarrao');
-function trocaCor(){
-    if(telao.style.backgroundColor == 'blue' )
-    document.getElementById('workspace').style.backgroundColor = 'rgb(97, 97, 57)';
-    else
-    document.getElementById('workspace').style.backgroundColor = 'blue';
+const telao = document.getElementById("workspace");
+const botaozin = document.getElementById("macarrao");
+function trocaCor() {
+  if (telao.style.backgroundColor == "rgb(221, 174, 228)")
+    document.getElementById("workspace").style.backgroundColor =
+      "rgb(97, 97, 57)";
+  else
+    document.getElementById("workspace").style.backgroundColor =
+      "rgb(221, 174, 228)";
 }
 
- function tela(){
-    if(botaozin == !show)
-    botaozin.classList.toggle('active')
-    else
-    botaozin.classList.toggle('active')
+function tela() {
+  if (botaozin == !show) botaozin.classList.toggle("active");
+  else botaozin.classList.toggle("active");
 }
